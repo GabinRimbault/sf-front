b4c0bd003ab6b75ebc3cf49a1ae376736293e457
GabinRimlbault <gabin.rimbault@icloud.fr>
2023-09-01 03:59:20 +0200

    https://github.com/users/GabinRimbault/projects/4?pane=issue&itemId=37407309 - Add table composant composite

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import React from "react";
 import "./lib/scss/all.css";
-import {Cards} from "./lib";
+import {BlocInformation, Cards, Wrapper} from "./lib";
 import GptCards from "./lib/components/modules/General/Cards/GptCards";
+import Table from "./lib/components/base/Table";
 
 function App() {
 
   return (
-    <>
-      <GptCards>
-        <Cards>
-          <Cards.Header>
-            Titre
-          </Cards.Header>
-          <Cards.Body>
-            Corps
-          </Cards.Body>
-          <Cards.Footer>
-            Pied
-          </Cards.Footer>
-        </Cards>
-
-      </GptCards>
-    </>
+    <Wrapper>
+      <Table>
+          <Table.Head classHeading={"infos bg-color"}>
+              <Table.HeadCell>
+                  Product Name
+              </Table.HeadCell>
+              <Table.HeadCell>
+                  Color
+              </Table.HeadCell>
+              <Table.HeadCell>
+                  Category
+              </Table.HeadCell>
+              <Table.HeadCell>
+                  Price
+              </Table.HeadCell>
+          </Table.Head>
+          <Table.Row>
+              <Table.Cell>1er contenu</Table.Cell>
+              <Table.Cell>2eme contenu</Table.Cell>
+              <Table.Cell>1er contenu</Table.Cell>
+              <Table.Cell>2eme contenu</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+              <Table.Cell>1er contenu</Table.Cell>
+              <Table.Cell>2eme contenu</Table.Cell>
+              <Table.Cell>1er contenu</Table.Cell>
+              <Table.Cell>2eme contenu</Table.Cell>
+          </Table.Row>
+      </Table>
+    </Wrapper>
   )
 }
 
