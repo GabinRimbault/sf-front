babdfea9371feafdd0fb284c3121e0e3fa0fac02
GabinRimlbault <gabin.rimbault@icloud.fr>
2023-09-03 02:48:30 +0200

    View Changelog v1.0.6.md

---
+++ src/lib/components/modules/General/Carousel/Carousel.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect, Children, useCallback } from 'react';
+
+export default function Carousel({ children, slideInterval = 5000 }) {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const images = Children.map(children, (child) => child.props.src);
+
+    const goToPreviousSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    };
+
+    const goToNextSlide = useCallback(() => { // Utilisez useCallback pour mémoriser la fonction
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, [images]);
+
+    useEffect(() => {
+        const carouselInterval = setInterval(goToNextSlide, slideInterval);
+
+        return () => {
+            clearInterval(carouselInterval);
+        };
+    }, [images, slideInterval, goToNextSlide]);
+
+    const currentImage = images[currentIndex];
+
+    return (
+        <div className="carousel">
+            <img src={currentImage} alt={`Slide ${currentIndex + 1}`} />
+            <div className="carousel-navigation">
+                <button className={"carousel-button"} onClick={goToPreviousSlide}>
+                    <i className="fas fa-chevron-left"></i>
+                </button>
+                <button className={"carousel-button"} onClick={goToNextSlide}>
+                    <i className="fas fa-chevron-right"></i>
+                </button>
+            </div>
+        </div>
+    );
+}
